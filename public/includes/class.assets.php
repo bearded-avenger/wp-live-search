efd348ede341a67653e2244d153e5bb6247cbbf2
Bearded Avenger <email@nickhaskins.com>
2015-06-12 14:22:29 -0500

    let styles be disabled with a define

--- public/includes/class.assets.php
+++ public/includes/class.assets.php
@@ -15,15 +15,19 @@ class wpSearchAssets{
 		$settings = array( 'root' => home_url( $home_url ), 'nonce' => wp_create_nonce( 'wp_json' ) );
 
 		// wp api client
-		wp_enqueue_script( 'wp-api-js', WP_SEARCH_URL.'/public/assets/js/util--wp-api.js', array( 'jquery', 'underscore', 'backbone' ), WP_SEARCH_VERSION, true );
+		wp_enqueue_script( 'wp-api-js', WP_LIVE_SEARCH_URL.'/public/assets/js/util--wp-api.js', array( 'jquery', 'underscore', 'backbone' ), WP_LIVE_SEARCH_VERSION, true );
 		wp_localize_script( 'wp-api-js', 'WP_API_Settings', $settings );
 
 		// wp search script
-		wp_enqueue_script('wpls-script', WP_SEARCH_URL.'/public/assets/js/wp-live-search.js', array('jquery'), WP_SEARCH_VERSION, true);
+		wp_enqueue_script('wpls-script', WP_LIVE_SEARCH_URL.'/public/assets/js/wp-live-search.js', array('jquery'), WP_LIVE_SEARCH_VERSION, true);
 		wp_localize_script('wpls-script','wp_search_vars', array('helperText' => __('one more character','wp-live-search')));
 
-		// wp seatch style
-		wp_enqueue_style('wpls-style', WP_SEARCH_URL.'/public/assets/css/style.css', WP_SEARCH_VERSION );
+		if ( !defined( 'WPLS_DISABLE_STYLE' ) ) {
+
+			// wp seatch style
+			wp_enqueue_style('wpls-style', WP_LIVE_SEARCH_URL.'/public/assets/css/style.css', WP_LIVE_SEARCH_VERSION );
+
+		}
 	}
 
 }
