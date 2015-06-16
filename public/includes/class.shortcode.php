eb999d15375d2ab8b611236d3e5da3a9d072d84c
Bearded Avenger <email@nickhaskins.com>
2015-06-15 19:15:11 -0500

    cpt support
    pass `type="books"` for a "books" custom post type search endpoint #4

--- public/includes/class.shortcode.php
+++ public/includes/class.shortcode.php
@@ -10,7 +10,7 @@ class wpSearchShortcode{
 	public function shortcode( $atts, $content = null ) {
 
 		$defaults = array(
-			'type'	 		=> 'posts', // 'posts', 'pages'
+			'type'	 		=> 'posts', // 'posts', 'pages', 'books'
 			'placeholder'	=> __('Search...','wp-live-search'),
 			'results' 		=> __('entries found','wp-live-search'),
 			'target'		=> ''
@@ -19,6 +19,8 @@ class wpSearchShortcode{
 
 		$results_text = $atts['results'] ? $atts['results'] : false;
 		$target       = $atts['target'] ? sprintf( 'data-target=%s', trim( $atts['target'] ) ) : false;
+
+		$type = 'posts' == $atts['type'] || 'pages' == $atts['type'] ? $atts['type'] : sprintf('posts?type=%s&', trim( $atts['type'] ) );
 
 		ob_start();
 
@@ -31,7 +33,7 @@ class wpSearchShortcode{
 			</div>
 
 			<div id="wpls--input-wrap">
-				<input itemprop="query-input" type="text" data-object-type="<?php echo esc_attr( trim( $atts['type'] ) );?>" id="wpls--input" placeholder="<?php echo esc_attr( $atts['placeholder'] );?>">
+				<input itemprop="query-input" type="text" data-object-type="<?php echo esc_attr( $type );?>" id="wpls--input" placeholder="<?php echo esc_attr( $atts['placeholder'] );?>">
 				<div id="wpls--loading" class="wpls--loading"><div class="wpls--loader"></div></div>
 			</div>
 
